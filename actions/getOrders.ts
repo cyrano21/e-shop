2b4c809c906b9e9e05d46a15bc96fb79bb739ab1
cyrano21 <louiscyrano@gmail.com>
2024-04-26 23:00:48 +0200

    go back restore users

--- actions/getOrders.ts
+++ actions/getOrders.ts
@@ -1,18 +1,23 @@
-import prisma from '@/libs/prismadb'
+import prisma from '@/libs/prismadb';
 
 export default async function getOrders(){
     try {
         const orders = await prisma.order.findMany({
-            include:{
+            include: {
                 user: true
             },
             orderBy: {
                 createDate: 'desc'
             }
-        })
+        });
 
-        return orders
-    } catch (error: any) {
-        throw new Error(error)
+        // Filtrer les commandes sans utilisateur si nécessaire
+        const filteredOrders = orders.filter(order => order.user !== null);
+
+        return filteredOrders;
+    } catch (error) {
+        console.error("Error fetching orders:", error);
+        // Renvoyer une réponse d'erreur plus générique
+        throw new Error("Failed to fetch orders.");
     }
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,19 +7,27 @@ import BarGraph from "./BarGraph";
 import getGraphData from "@/actions/getGraphData";
 
 const Admin = async () => {
-  const products = await getProducts({category: null})
-  const orders = await getOrders()
-  const users = await getUsers()
-  const graphData = await getGraphData()
-  
-  return <div className="pt-8">
-    <Container>
-      <Summary products={products} orders={orders} users={users}/>
-      <div className="mt-4 mx-auto max-w-[1150px]">
-        <BarGraph data = {graphData}/>
-      </div>
-    </Container>
-    </div>;
+  try {
+    const products = await getProducts({category: null});
+    const orders = await getOrders();
+    const users = await getUsers();
+    const graphData = await getGraphData();
+
+    return (
+        <div className="pt-8">
+          <Container>
+            <Summary products={products} orders={orders} users={users}/>
+            <div className="mt-4 mx-auto max-w-[1150px]">
+              <BarGraph data={graphData}/>
+            </div>
+          </Container>
+        </div>
+    );
+  } catch (error) {
+    console.error("Failed to load admin data:", error);
+    return <div>Error loading data</div>; // Afficher un message d'erreur approprié
+  }
 };
+
 
 export default Admin;
