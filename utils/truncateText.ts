b80387ab53fa6d0bb37e5230e47ad433e0a15b0b
cyrano21 <louiscyrano@gmail.com>
2024-04-27 18:42:37 +0200

    change truncteText

--- utils/truncateText.ts
+++ utils/truncateText.ts
@@ -1,6 +1,7 @@
 // Modification de la fonction pour accepter un second paramètre spécifiant la longueur de troncature
-export const truncateText = (str: string, maxLength: number) => {
+export const truncateText = (str: string, maxLength: number = 25) => {
   if (str.length < maxLength) return str;
   return str.substring(0, maxLength) + "...";
 };
+
 
