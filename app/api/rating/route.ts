a51164943f526fa94e5c1090b425817da118a28d
cyrano21 <louiscyrano@gmail.com>
2024-04-27 12:04:20 +0200

    change raating page

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -1,51 +1,36 @@
-// Importation des dépendances
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/libs/prismadb'; // Assurez-vous que le chemin d'importation est correct
 import getCurrentUser from "@/actions/getCurrentUser";
+import { Review } from "@prisma/client";
+import { NextResponse } from "next/server";
 
-// Handler pour la méthode POST
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method Not Allowed' });
-    }
 
+export async function POST(request: Request){
     const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-        return res.status(401).json({ error: "Authentication required" });
+    if(!currentUser){
+        return NextResponse.error();
     }
 
-    const { comment, rating, product, userId } = req.body;
+    const body = await request.json()
+    const {comment, rating, product, userId} = body;
 
-    if (!comment || typeof rating !== 'number' || !product || !product.id || !userId) {
-        return res.status(400).json({ error: "Missing required fields" });
+    const deliveredOrder = currentUser?.orders.some(order => order.products.find(item => item.id === product.id) && order.deliveryStatus === 'delivered')
+
+    const userReview = product?.reviews.find(((review: Review) =>{
+        return review.userId === currentUser.id
+    }))
+
+    if(userReview || !deliveredOrder){
+        return NextResponse.error()
     }
 
-    const deliveredOrder = currentUser?.orders.some(order =>
-        order.products.find(item => item.id === product.id) && order.deliveryStatus === 'delivered');
+    const review = await prisma?.review.create({
+        data: {
+            comment,
+            rating,
+            productId: product.id,
+            userId
+        }
+    })
 
-    const userReview = product?.reviews.find((review: { userId: string; }) => review.userId === currentUser.id);
-
-    if (userReview) {
-        return res.status(403).json({ error: "Review already exists" });
-    }
-
-    if (!deliveredOrder) {
-        return res.status(403).json({ error: "No delivered order found for the product" });
-    }
-
-    try {
-        const review = await prisma.review.create({
-            data: {
-                comment,
-                rating,
-                productId: product.id,
-                userId
-            }
-        });
-        return res.status(201).json(review);
-    } catch (error: any) {
-        console.error('Error saving rating:', error);
-        return res.status(500).json({ error: 'Internal Server Error', details: error.message });
-    }
+    return NextResponse.json(review)
 }
