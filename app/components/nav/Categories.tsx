c628a66ae1930806ed76fc634834f89321475b2a
cyrano21 <louiscyrano@gmail.com>
2024-04-29 11:38:48 +0200

    add burger

--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -1,32 +1,81 @@
-'use client'
 
+"use client"
+
+// Categories.js
+import React, { useState } from 'react';
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import { categories } from "@/utils/Categories";
 import Container from "../Container";
 import Category from "./Category";
-import { usePathname, useSearchParams } from "next/navigation";
+import { Menu } from 'lucide-react';
+import queryString from "query-string";
 
 const Categories = () => {
-    const params = useSearchParams()
-    const category = params?.get('category')
+    const [menuOpen, setMenuOpen] = useState(false);
+    const params = useSearchParams();
+    const router = useRouter();
+    const categoryQuery = params?.get('category');
     const pathname = usePathname();
-    const isMainPage = pathname === '/'
+    const isMainPage = pathname === '/';
 
-    if(!isMainPage) return null;
+    const handleCategorySelect = (category: { label: any; }) => {
+        let currentQuery = {};
+        if (params) {
+            currentQuery = queryString.parse(params.toString());
+        }
+        const updatedQuery = {
+            ...currentQuery,
+            category: category.label
+        };
+        const url = queryString.stringifyUrl({
+            url: '/',
+            query: updatedQuery
+        }, { skipNull: true });
+        router.push(url);
+        setMenuOpen(false); // Close the menu after selection
+    };
 
-    return (<div className="bg-white">
-        <Container>
-            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
-                {categories.map((item) =>(
-                    <Category
-                        key={item.label}
-                        label={item.label}
-                        icon={item.icon}
-                        selected= {category === item.label || (category === null && item.label === 'All')}
+    if (!isMainPage) return null;
+
+    return (
+        <div className="bg-white">
+            <Container>
+                <div className="lg:hidden">
+                    <Menu
+                        className="cursor-pointer text-gray-700 hover:text-gray-900"
+                        size={24}
+                        onClick={() => setMenuOpen(!menuOpen)}
                     />
-                ))}
-            </div>
-        </Container>
-    </div> );
-}
- 
+                    {menuOpen && (
+                        <div
+                            className="absolute top-full left-0 mt-2 w-48 bg-white shadow-lg rounded-lg border p-2 z-50">
+                            {categories.map((item) => (
+                                <div
+                                    key={item.label}
+                                    onClick={() => router.push(`/?category=${item.label}`)}
+                                    className={`p-2 hover:bg-gray-100 cursor-pointer
+                                    ${categoryQuery === item.label ? 'text-blue-700 font-bold' : 'text-gray-900'}
+                                    `}
+                                >
+                                    {item.label}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <div className="hidden lg:flex pt-4 flex-row items-center justify-between overflow-x-auto">
+                    {categories.map((item) => (
+                        <Category
+                            key={item.label}
+                            label={item.label}
+                            icon={item.icon}
+                            selected={categoryQuery === item.label || (categoryQuery === null && item.label === 'All')}
+                        />
+                    ))}
+                </div>
+            </Container>
+        </div>
+    );
+};
+
 export default Categories;
