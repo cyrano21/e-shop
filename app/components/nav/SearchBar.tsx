2dbfc445484ae4ba2202b1caaff8ee138bdb2ceb
cyrano21 <louiscyrano@gmail.com>
2024-04-29 03:41:04 +0200

    add css

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { useRouter } from "next/navigation";
 import queryString from "query-string";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
@@ -16,32 +15,36 @@ const SearchBar = () => {
         defaultValues: {
             searchTerm: ''
         }
-    })
+    });
 
-    const onSubmit: SubmitHandler<FieldValues> = async (data) =>{
-        if(!data.searchTerm) return router.push('/')
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        if (!data.searchTerm) return router.push('/');
 
         const url = queryString.stringifyUrl({
             url: '/',
-            query:{
+            query: {
                 searchTerm: data.searchTerm
             }
-        },{skipNull: true})
+        }, { skipNull: true });
 
-        router.push(url)
-        reset()
-    }
+        router.push(url);
+        reset();
+    };
 
-    return ( <div className="flex items-center">
-        <input
-        {...register('searchTerm')}
-        autoComplete="off"
-        type="text"
-        placeholder="Explorer E~Shop"
-        className="p-2 border border-gray-300 rounded-l-md focus:outline-none focus:bourder-[0.5px] focus:border-slate-500 w-80"
-        />
-        <button onClick={handleSubmit(onSubmit)} className="bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md">Recherche</button>
-    </div> );
+    return (
+        <div className="flex items-center">
+            <input
+                {...register('searchTerm')}
+                autoComplete="off"
+                type="text"
+                placeholder="Explorer E~Shop"
+                className="input-style p-2 border border-gray-300 rounded-l-md w-80"
+            />
+            <button onClick={handleSubmit(onSubmit)} className="bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md">
+                Rechercher
+            </button>
+        </div>
+    );
 }
 
 export default SearchBar;
