87d4806b4936de015fb0241ab1f8111b97fa191b
cyrano21 <louiscyrano@gmail.com>
2024-04-29 12:59:40 +0200

    shrink searchBar

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import queryString from "query-string";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import {FiSearch} from "react-icons/fi";
 
 const SearchBar = () => {
     const router = useRouter();
@@ -41,8 +42,12 @@ const SearchBar = () => {
                 placeholder="Explorer E~Shop"
                 className="input-style flex-grow p-2 border border-gray-300 rounded-l-md focus:outline-none"
             />
-            <button onClick={handleSubmit(onSubmit)} className="bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md">
-                Rechercher
+            <button onClick={handleSubmit(onSubmit)}
+                    className="bg-slate-700 hover:opacity-80 text-white px-3 py-2 rounded-r-md flex items-center h-10">
+
+                <span
+                    className="hidden sm:inline">Rechercher</span>
+                <FiSearch className="text-lg sm:hidden"/>
             </button>
         </div>
     );
