34ccb6a8c40f89871d28c4dd7dc652ee69736dff
cyrano21 <louiscyrano@gmail.com>
2024-04-29 04:59:52 +0200

    add bg css

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -6,7 +6,7 @@ const HomeBanner = () => {
       <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
         <div className="mb-8 md:mb-0 text-center">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
-            Solde d'été
+            Solde d&rsquo; été
           </h1>
           <p className="text-lg md:text-xl text-white mb-2">
             Profitez de réductions sur les articles sélectionnés
--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -4,39 +4,37 @@ import Avatar from "@/app/components/Avatar";
 import Heading from "@/app/components/Heading";
 import { Rating } from "@mui/material";
 import moment from "moment";
+import React from "react";
 
 interface ListRatingProps {
-  product: any;
+    product: any;
 }
 
 const ListRating: React.FC<ListRatingProps> = ({ product }) => {
- if(product.reviews.length === 0) return null
-  return (
-    <div>
-      <Heading title="Product Review" />
-      <div className="text-sm mt-2">
-        {product.reviews &&
-          product.reviews.map((review: any) => {
-            return (
-              <div key={review.id} className="max-w-[300px]">
-                <div className="flex gap-2 items-center">
-                  <Avatar src={review?.user.image} />
-                  <div className="font-semibold">{review?.user.name}</div>
-                  <div className="font-light">
-                    {moment(review.createdDate).fromNow()}
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <Rating value={review.rating} readOnly />
-                  <div className="ml-2">{review.comment}</div>
-                  <hr className="mt-4 mb-4" />
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
+    if (product.reviews.length === 0) return null;
+
+    return (
+        <div className="list-rating-container">
+            <Heading title="Évaluation du produit" className="list-rating-title" />
+            <div className="text-sm mt-2">
+                {product.reviews.map((review: any) => (
+                    <div key={review.id} className="list-rating-review max-w-[300px]">
+                        <div className="flex gap-2 items-center">
+                            <Avatar src={review?.user.image} />
+                            <div className="font-semibold">{review?.user.name}</div>
+                            <div className="font-light list-rating-subtext">
+                                {moment(review.createdDate).fromNow()}
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <Rating value={review.rating} readOnly />
+                            <div className="ml-2">{review.comment}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default ListRating;
