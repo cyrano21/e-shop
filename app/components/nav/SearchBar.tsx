750debb945e209935ace777b6b2403c1cf1ecb13
cyrano21 <louiscyrano@gmail.com>
2024-04-29 13:27:55 +0200

    add FiSearch in  searchBar

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -1,3 +1,4 @@
+
 'use client'
 
 import { useRouter } from "next/navigation";
@@ -34,24 +35,15 @@ const SearchBar = () => {
     };
 
     return (
-        <div className="flex items-center w-full sm:w-56 md:w-64 lg:w-96">
-            <input
-                {...register('searchTerm')}
-                autoComplete="off"
-                type="text"
-                placeholder="Explorer E~Shop"
-                className="input-style flex-grow p-2 border border-gray-300 rounded-l-md focus:outline-none"
-            />
-            <button onClick={handleSubmit(onSubmit)}
-                    className="bg-slate-700 hover:opacity-80 text-white px-3 py-2 rounded-r-md flex items-center h-10">
-
-                <span
-                    className="hidden sm:inline">Rechercher</span>
-                <FiSearch className="text-lg sm:hidden"/>
+        <div className="flex items-center w-full min-w-0">
+            <input  placeholder="Explorer E~Shop"
+                    className="flex-grow input-style p-1 border border-gray-300 rounded-l-md focus:outline-none h-10 w-full max-w-xs"/>
+            <button className="bg-slate-700 hover:opacity-80 text-white px-2 py-1 rounded-r-md h-10">
+                <span className="hidden sm:inline">Rechercher</span>
+                <FiSearch className="sm:hidden"/>
             </button>
         </div>
     );
 }
 
-export default SearchBar;
-
+export default SearchBar
